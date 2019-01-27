932f35031723563c79ecf450e079a33971b2fb56
ruchamahabal <ruchamahabal2@gmail.com>
2019-01-28 00:42:10 +0530

    navbar mega menu

--- header.php
+++ header.php
@@ -12,98 +12,96 @@
     <!-- custom css -->
     <link rel="stylesheet" href="css/custom.css">
     
-    <!-- rucha -->
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
 	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    
-    <!-- mega menu script -->
-    <script type="text/javascript">
-              $(function () {
-                $('.toggle-menu').click(function(){
-                  $('.exo-menu').toggleClass('display');
-                });
-              });
-    </script>
+		
+		<!-- Optional JavaScript -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
   </head>
   <body>
-    <!-- mega menu -->
-	 <!-- <div class="content">
-	 
-		<ul class="exo-menu">
-			<li><a class="active" href="#"><i class="fa fa-home"></i> Home</a></li>
-			<li class="mega-drop-down"><a href="#"><i class="fas fa-user"></i>For Me</a>
-				<div class="animated fadeIn mega-menu">
-					<div class="mega-menu-wrap">
-						<div class="row">
-							<div class="col-md-3">
-              <h4 class="row mega-title">Cash In My A/C</h4>
-              <h4 class="row mega-title">Personal Loan</h4>
-								<ul class="icon-des">
-									<li><a href="#"><i class="fas fa-plane"></i>Travel</a></li>
-									<li><a href="#"><i class="fas fa-graduation-cap"></i>Education</a></li>
-									<li><a href="#"><i class="fas fa-heart"></i>Wedding</a></li>
-									<li><a href="#"><i class="fas fa-wheelchair"></i>Medical Emergency</a></li>
-                  <li><a href="#"><i class="fas fa-house-damage"></i>Home Renovation/Expansion</a></li>
-                  <li><a href="#"><i class="far fa-building"></i>Loan Against Property</a></li>
-								</ul>
-              </div>
-              <div class="col-md-3">
-							<h4 class="row mega-title">Buy</h4>
-								<ul class="icon-des">
-									<li><a href="#"><i class="fas fa-motorcycle"></i>2 Wheeler</a></li>
-									<li><a href="#"><i class="fas fa-mobile-alt"></i>Mobile</a></li>
-									<li><a href="#"><i class="fas fa-home"></i>Home Loan</a></li>
-									<li><a href="#"><i class="fas fa-car"></i>Pre-Owned Car</a></li>
-								</ul>
-              </div>
-						</div>
-					</div>	
-				</div>
-      </li>
-      <li class="mega-drop-down"><a href="#"><i class="fas fa-briefcase"></i>For my Business</a>
-				<div class="animated fadeIn mega-menu">
-					<div class="mega-menu-wrap">
-						<div class="row">
-							<div class="col-md-3">
-              <h4 class="row mega-title">Cash In My A/C</h4>
-                <h4 class="row mega-title">Business Loans</h4>
-								<ul class="icon-des">
-									<li><a href="#"><i class="fas fa-chart-line"></i>Business Expansion</a></li>
-                </ul>
-                <h4 class="row mega-title">Working Capital</h4>
-								<ul class="icon-des">
-									<li><a href="#"><i class="far fa-credit-card"></i>Merchant Cash Advance</a></li>
-								</ul>
-              </div>
-              <div class="col-md-3">
-							<h4 class="row mega-title">Buy/Lease</h4>
-								<ul class="icon-des">
-									<li><a href="#"><i class="fas fa-file-medical-alt"></i>Healthcare Equipment</a></li>
-								</ul>
-              </div>
-						</div>
-					</div>	
-				</div>
-      </li>
-      <li class="mega-drop-down"><a href="#"><i class="fas fa-calculator"></i>EMI Calculators</a>
-				<div class="animated fadeIn mega-menu">
-					<div class="mega-menu-wrap">
-						<div class="row">
-							<div class="col-md-3">
-              <h4 class="row mega-title">EMI Calculators</h4>
-								<ul class="icon-des">
-									<li><a href="#"><i class="fas fa-hand-holding-usd"></i>Personal Loan Calculator</a></li>
-									<li><a href="#"><i class="fas fa-home"></i>Home Loan Calculator</a></li>
-									<li><a href="#"><i class="fas fa-car"></i>Vehicle Loan Calculator</a></li>
-									<li><a href="#"><i class="fas fa-user-tie"></i>Business Loan Calculator</a></li>
-								</ul>
-              </div>		
-						</div>
-					</div>	
-				</div>
-			</li>
-			<a href="#" class="toggle-menu visible-xs-block">|||</a>	
-</ul> 
-	 </div> -->
-<!-- /mega menu -->
+<!-- mega menu -->
+<header class="site-header" role="banner">
+	<nav class="navbar navbar-expand-sm">
+		<!-- <img id="navbar-logo" src="assets/images/logo.jpeg" width="100" height="80" alt=""> -->
+			<button class="navbar-toggler mr-2" type="button" data-toggle="collapse" data-target="#navbar">
+					<span class="navbar-toggler-icon"></span>
+			</button>
+			<span class="navbar-brand d-flex flex-fill">Brand</span>
+			<div class="navbar-collapse collapse" id="navbar">
+					<ul class="navbar-nav justify-content-center d-flex flex-fill">
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									For me
+								</a>
+								<div class="dropdown-menu mega-menu" aria-labelledby="navbarDropdown">
+								<div class="row">
+										<div class="col">
+											<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Cash In My Account</a>
+											<div class="dropdown-divider"></div>
+											<h6 class="nav-sub-heading">Personal Loan</h6>
+											<a class="dropdown-item" href="#"><i class="fas fa-plane"></i>Travel</a>
+											<a class="dropdown-item" href="#"><i class="fas fa-graduation-cap"></i>Education</a>
+											<a class="dropdown-item" href="#"><i class="fas fa-heart"></i>Wedding</a>
+											<a class="dropdown-item" href="#"><i class="fas fa-wheelchair"></i>Medical Emergency</a>
+											<a class="dropdown-item" href="#"><i class="fas fa-house-damage"></i>Home Renovation/Expansion</a>
+											<a class="dropdown-item" href="#"><i class="far fa-building"></i>Loan Against Property</a>
+										</div>
+										<div class="col">
+											<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Buy</a>
+											<div class="dropdown-divider"></div>
+											<a class="dropdown-item" href="#"><i class="fas fa-motorcycle"></i>2 Wheeler</a>
+											<a class="dropdown-item" href="#"><i class="fas fa-mobile-alt"></i>Mobile</a>
+											<a class="dropdown-item" href="#"><i class="fas fa-home"></i>Home Loan</a>
+											<a class="dropdown-item" href="#"><i class="fas fa-car"></i>Pre-Owned Car</a>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									For my business
+								</a>
+								<div class="dropdown-menu mega-menu" aria-labelledby="navbarDropdown">
+									<div class="row">
+											<div class="col">
+												<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Cash In The A/C</a>
+												<div class="dropdown-divider"></div>
+												<h6 class="nav-sub-heading">Business Loans</h6>
+												<a class="dropdown-item" href="#"><i class="fas fa-chart-line"></i>Business Expansion</a>
+												<h6 class="nav-sub-heading">Working Capital</h6>
+												<a class="dropdown-item" href="#"><i class="far fa-credit-card"></i>Merchant Cash Advance</a>
+											</div>
+											<div class="col">
+												<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Buy/Lease</a>
+												<div class="dropdown-divider"></div>
+												<a class="dropdown-item" href="#"><i class="fas fa-file-medical-alt"></i>Healthcare Equipment</a>
+											</div>
+										</div>
+								</div>
+							</li>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							 			EMI Calculators
+								</a>
+								<div class="dropdown-menu dropdown-menu-right mega-menu" aria-labelledby="navbarDropdown">
+										<div class="row">
+											<div class="col">
+												<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">EMI Calculators</a>
+												<div class="dropdown-divider"></div>
+												<a class="dropdown-item" href="#"><i class="fas fa-hand-holding-usd"></i>Personal Loan Calculator</a>
+												<a class="dropdown-item" href="#"><i class="fas fa-home"></i>Home Loan Calculator</a>
+												<a class="dropdown-item" href="#"><i class="fas fa-car"></i>Vehicle Loan Calculator</a>
+												<a class="dropdown-item" href="#"><i class="fas fa-user-tie"></i>Business Loan Calculator</a>
+											</div>
+										</div>
+								</div>
+							</li>
+					</ul>
+			</div>
+			<div class="d-flex flex-fill"><!--spacer--> </div>
+	</nav>
+</header>
