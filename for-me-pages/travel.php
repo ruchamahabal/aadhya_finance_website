86d705901caf6463b8f447222db9e4643afb6906
root <cybersagex@gmail.com>
2019-02-02 13:41:47 +0530

    completed travel page

--- for-me-pages/travel.php
+++ for-me-pages/travel.php
@@ -1,26 +1,43 @@
 <?php include '../header.php'; ?>
+<div class="container">
+
+<section>
+  <div class="row">
+    <div class="col">
+      <div class="container">
+        <h2>Personal Loan for Travel</h2>
+        <p class="lead">Don’t let lack of money dampen your wanderlust. Take that solo vacation, family holiday, or long overdue honeymoon, because with our quick and easy personal loans for travel, all you need to do is start packing!</p>
+        <a href="#" class="btn btn-danger">Apply Now</a>
+      </div>
+    </div>
+    <div class="col">
+      <!-- animation will come here -->
+    </div>
+  </div>
+</section>
+
 
 <section id="services">
-     <h3 class="heading" align="center">You’ve arrived at the ultimate financing destination!</h3>
-     <p align="center">Be it the big things in life or the little joys, you should have it all. Our simple, fast, and accessible business and personal loans<br> make sure nothing holds you back.</p>
+     <h3 class="heading" align="center">Here's what we have to offer</h3>
+
      <div class="container">
         <div class="row">
             <div class="col-md-4 col-sm-8">
                 <div class="serviceBox blue">
                     <div class="service-icon">
-                        <i class="fas fa-users"></i>
+                        <i class="fas fa-rupee-sign"></i>
                     </div>
-                    <h3 class="title">Aadhya For All</h3>
+                    <h3 class="title">Loans from Rs. 1–5 lakhs</h3>
                     <p class="description">
-                        If you have a dream, we’ll fix it for you. We’re working towards offering more loans to more people
+                        Whether you’re planning to holiday abroad or relax at a cozy Goan resort, we won’t let money come in the way of a perfect vacation.
                     </p>
-                    <a class="read-more" href="">Read More</a>
+                    <!-- <a class="read-more" href="">Read More</a> -->
                 </div>
             </div>
             <div class="col-md-4 col-sm-8">
                 <div class="serviceBox">
                     <div class="service-icon">
-                        <i class="fas fa-laptop-code"></i>
+                        <i class="fas fa-wallet"></i>
                     </div>
                     <h3 class="title">Aadhya Digi-way</h3>
                     <p class="description">
@@ -32,7 +49,7 @@
             <div class="col-md-4 col-sm-8">
                 <div class="serviceBox gray">
                     <div class="service-icon">
-                        <i class="fas fa-rupee-sign"></i>
+                        <i class="fas fa-check-circle"></i>
                     </div>
                     <h3 class="title">Aadhya Express</h3>
                     <p class="description">
@@ -44,5 +61,95 @@
         </div>
     </div>
 </section>
+<section id="requirements">
+  <div class="row">
+    <div class="col">
+      <h5>How do I check my eligibility for a personal loan for travel from Clix?</h5>
+      <p class="lead bold">If you’re</p>
+      <p>-Over 21 and under 58, while applying for the loan</p>
+      <p>-A salaried employee, with at least 6 months of experience in current company and total professional experience of at least 12 months</p>
+      <p>-Earning a minimum income of Rs. 20,000 per month</p>
+      <p class="lead bold">You’re eligible!</p>
+    </div>
+    <div class="col">
+      <!-- image will be added here  -->
+    </div>
+  </div>
+</section>
+<section>
+  <h3 class="heading" align="center">What documents do I need to submit to apply for a personal loan for travel from Aadhyafinance?</h3>
+  <div class="row">
+    <div class="col">
+      <center>
+      <div class="card border-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">
+            <img style="max-width:60px;" src="../assets/images/form.svg" alt="">
+        </div>
+        <div class="card-body">
+          <h6 class="card-title">Completed application form</h6>
+          <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+        </div>
+      </div>
+      </center>
+    </div>
+    <div class="col">
+      <center>
+      <div class="card border-danger mb-3" style="max-width: 20rem;">
+        <div class="card-header">
+            <img style="max-width:60px;" src="../assets/images/picture.svg" alt="">
+        </div>
+        <div class="card-body">
+          <h6 class="card-title">Recent passport-size photographs</h6>
+          <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+        </div>
+      </div>
+      </center>
+    </div>
+    <div class="col">
+      <center>
+      <div class="card border-success mb-3" style="max-width: 20rem;">
+        <div class="card-header">
+            <img style="max-width:60px;" src="../assets/images/fingerprint.svg" alt="">
+        </div>
+        <div class="card-body">
+          <h6 class="card-title">Identity (ID) Proof</h6>
+          <p class="card-text">Aadhaar card, PAN card or Government ID (for government employees)</p>
+        </div>
+      </div>
+      </center>
+    </div>
+    <div class="col">
+      <center>
+      <div class="card border-info mb-3" style="max-width: 20rem;">
+        <div class="card-header">
+            <img style="max-width:60px;" src="../assets/images/house.svg" alt="">
+        </div>
+        <div class="card-body">
+          <h6 class="card-title">Address Proof</h6>
+          <p class="card-text">Aadhaar card, in case current address is not updated in Aadhaar card, one certified copy of Official Valid Documents (OVD) is required. OVDs are: Passport, Voter’s ID, Driving License</p>
+        </div>
+      </div>
+      </center>
+    </div>
+    <div class="col">
+      <center>
+      <div class="card border-warning mb-3" style="max-width: 20rem;">
+        <div class="card-header">
+            <img style="max-width:60px;" src="../assets/images/wallet.svg" alt="">
+        </div>
+        <div class="card-body">
+          <h6 class="card-title">Income Proof</h6>
+          <p class="card-text">
 
+    Latest One month Pay slip or Form 16 for the current year
+
+</p>
+        </div>
+      </div>
+      </center>
+    </div>
+  </div>
+</section>
+
+</div>
 <?php include '../footer.php'; ?>
